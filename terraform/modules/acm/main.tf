8aac3220c21a3608c35b46b80eefeef6fe29fa34
mazin1hn <mazinwork@outlook.com>
2026-01-13 19:24:34 +0000

    Add ACM module

---
+++ terraform/modules/acm/main.tf
@@ -0,0 +1,18 @@
+#ACM : Requesting certification for domain
+
+resource "aws_acm_certificate" "ecs" {
+  domain_name       = var.acm_certificate_domain_name
+  validation_method = var.acm_certificate_validation_method
+
+  lifecycle {
+    create_before_destroy = var.acm_create_before_destroy
+  }
+}
+
+#Wait for cert to be issued 
+
+resource "aws_acm_certificate_validation" "ecs-cert-validation" {
+    validation_record_fqdns = [ cloudflare_dns_record.domain_validation_record.name ]
+    certificate_arn = aws_acm_certificate.ecs.arn
+  
+}
---
+++ terraform/modules/acm/outputs.tf
@@ -0,0 +1,26 @@
+#ACM Validation Outputs 
+
+output "acm_certificate_record_name" {
+    description = "The record name of the acm certificate"
+    value =  aws_acm_certificate.ecs.domain_validation_options[0].resource_record_name
+  
+}
+
+output "acm_certificate_record_type" {
+    description = "The record type of the acm certificate"
+    value =  aws_acm_certificate.ecs.domain_validation_options[0].resource_record_type
+  
+}
+
+output "acm_certificate_record_value" {
+    description = "The record value of the acm certificate"
+    value =  aws_acm_certificate.ecs.domain_validation_options[0].resource_record_value
+  
+}
+
+output "acm_certificate_arn" {
+  description = "ARN of the ACM certificate"
+  value       = aws_acm_certificate.ecs.arn
+}
+
+
